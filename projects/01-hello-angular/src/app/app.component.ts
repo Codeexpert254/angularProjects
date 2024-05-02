feab8e65d06f42708b3ebd49aa017eb050ea87aa
Codeexpert254 <masaikipkorir@gmail.com>
2024-05-03 00:31:42 +0300

    First Commit

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,16 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>Books</li>
+      <li>Songs</li>
+      <li>Movies</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+  ol{
+    list-style-type: upper-roman;
+  }
+  `
 })
 export class AppComponent {}
